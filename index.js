2497dbecc1d292bcf40680cbec70dd0c92f1dbae
Branon Eusebio <BranonEusebio@charterhcg.com>
2020-06-05 12:17:35 -0700

    Tweaked UI animations on menu changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const menuChanger = document.querySelector('.fa-chevron-circle-right');
 const menu = document.querySelector('.side-nav');
 const menuElements = document.querySelector('ul');
-const workspace = document.querySelector('.workspace');
+const logo = document.querySelector('.logo');
 
 menuChanger.addEventListener('click', () => {
     menu.classList.toggle('side-nav-clicked');
     menuChanger.classList.toggle('menu-changer-clicked');
     menuChanger.classList.toggle('menu-changer');
+    logo.classList.toggle('slide-left');
 });
