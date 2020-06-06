330aab25e08b645bab5b51dee2974388f6186cae
Branon Eusebio <BranonEusebio@charterhcg.com>
2020-06-05 17:19:11 -0700

    Added burger menu with click listener and menu expansion animation

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const menu = document.querySelector('.side-nav');
 const menuElements = document.querySelector('ul');
 const logo = document.querySelector('.logo');
 
+const hamburger = document.querySelector('.hamburger-menu');
+const mobileMenu = document.querySelector('.mobileMenu');
+
+// Desktop Menu should be hidden or restored on click
 menuChanger.addEventListener('click', () => {
     menu.classList.toggle('side-nav-clicked');
     menuChanger.classList.toggle('menu-changer-clicked');
     menuChanger.classList.toggle('menu-changer');
     logo.classList.toggle('slide-left');
 });
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('extendMenuContainer');
+    menu.classList.toggle('shrinkMenuContainer');
+})
