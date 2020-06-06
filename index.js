74b5f3892da04e0bb73c835353d9bf1bb9a71cd1
Branon Eusebio <BranonEusebio@charterhcg.com>
2020-06-05 17:35:23 -0700

    Wrote awesome hamburger animation for mobile menu

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// Desktop menu elements
 const menuChanger = document.querySelector('.fa-chevron-circle-right');
 const menu = document.querySelector('.side-nav');
 const menuElements = document.querySelector('ul');
 const logo = document.querySelector('.logo');
-
+//hamburger Menu elements
 const hamburger = document.querySelector('.hamburger-menu');
 const mobileMenu = document.querySelector('.mobileMenu');
+const burgerOne = document.querySelector('.burger1');
+const burgerTwo = document.querySelector('.burger2');
+const burgerThree = document.querySelector('.burger3');
 
 // Desktop Menu should be hidden or restored on click
 menuChanger.addEventListener('click', () => {
@@ -15,6 +19,10 @@ menuChanger.addEventListener('click', () => {
 });
 
 hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-menu-flip');
     menu.classList.toggle('extendMenuContainer');
     menu.classList.toggle('shrinkMenuContainer');
+    burgerOne.classList.toggle('burger1flip');
+    burgerTwo.classList.toggle('burger2flip');
+    burgerThree.classList.toggle('burger3flip');
 })
